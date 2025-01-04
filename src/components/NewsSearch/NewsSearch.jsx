600d46057e7ec9ed6906918e745fd00c8ae4846f
Cavour Poon <cavouriypoon@outlook.com>
2025-01-05 00:20:45 +0800

    Make NewSearch responsive

--- src/components/NewsSearch/NewsSearch.jsx
+++ src/components/NewsSearch/NewsSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import NewsSearchResult from './NewsSearchResult/NewsSearchResult';
+import NewsSearchResult from '../NewsSearchResult/NewsSearchResult';
+import './NewsSearch.css';
 
 const API_KEY = process.env.REACT_APP_API_X_FUNCTIONS_KEY;
 const MIN_CHARACTERS = 6;
@@ -39,8 +40,13 @@ const NewsSearch = () => {
     };
 
     const handleSearch = async () => {
-        setNewsFeed([]); // Clean up previous results
         setSearchTextSuggestions([]); // Close the dropdown of suggestion text
+        if (query.trim().length === 0) {
+
+            alert('Please enter a search query');
+            return;
+        }
+        setNewsFeed([]); // Clean up previous results
         try {
             const response = await fetch(`https://one-leiaws-fa-python.azurewebsites.net/api/bing-news-search?q=${query}`, {
                 headers: {
@@ -62,26 +68,24 @@ const NewsSearch = () => {
     };
 
     return (
-        <div>
+        <div className="news-search-container">
             <h1>News Search</h1>
             <input
                 type="text"
                 placeholder="Search news..."
-                style={{ width: '400px' }}
+                className="news-search-input"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 onKeyDown={handleKeyDown}
             />
-            <button onClick={handleSearch}>Search</button>
+            <button onClick={handleSearch} className="news-search-button">Search</button>
             {searchTextSuggestions.length > 0 && (
-                <ul style={{ border: '1px solid #ccc', maxHeight: '150px', overflowY: 'auto', padding: '0', margin: '0', listStyleType: 'none' }}>
+                <ul className="suggestions-list">
                     {searchTextSuggestions.map((suggestion, index) => (
                         <li
                             key={index}
                             onClick={() => handleSuggestionClick(suggestion)}
-                            style={{ padding: '8px', cursor: 'pointer', backgroundColor: '#fff' }}
-                            onMouseOver={(e) => e.target.style.backgroundColor = '#f0f0f0'}
-                            onMouseOut={(e) => e.target.style.backgroundColor = '#fff'}
+                            className="suggestion-item"
                         >
                             {suggestion}
                         </li>
