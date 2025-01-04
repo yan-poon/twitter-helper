b7de93a1865645996b9ca5c71c172504d22ff19c
Cavour Poon <cavouriypoon@outlook.com>
2025-01-05 00:03:51 +0800

    Minor Enhancement

--- src/components/TwitterShare.jsx
+++ src/components/TwitterShare.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const TwitterShare = ({ tweetInfo, news }) => {
+    const textareaRef = useRef(null);
+
     const shareOnTwitter = () => {
-        const tweetText = `${tweetInfo.tweet}\n${tweetInfo.suggestHashtag.join(' ')}\n${news.url}`;
+        const tweetText = textareaRef.current.value;
         const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
         window.open(twitterUrl, '_blank');
     };
 
     return (
         <div style={{ marginTop: '10px' }}>
-            <textarea
-                value={`${tweetInfo.tweet}\n${tweetInfo.suggestHashtag.join(' ')}\n${news.url}`}
-                readOnly
-                rows={10}
-                style={{ width: '100%', padding: '10px', boxSizing: 'border-box' }}
-            />
-            <button onClick={shareOnTwitter} style={{ marginTop: '10px' }}>Share on Twitter</button>
+            <div>
+                <h4>Usefulness:</h4>
+                <p>{`${tweetInfo.informative}/10 (10 out of 10 implies must share)`}</p>
+                <h4>Summary</h4>
+                <p>{tweetInfo.summary}</p>
+            </div>
+            <div>
+                <h4>Tweet suggested by AI</h4>
+                <textarea
+                    ref={textareaRef}
+                    value={`${tweetInfo.tweet}\n${tweetInfo.suggestHashtag.join(' ')}\n${news.url}`}
+                    readOnly
+                    rows={10}
+                    style={{ width: '100%', padding: '10px', boxSizing: 'border-box' }}
+                />
+                <button onClick={shareOnTwitter} style={{ marginTop: '10px' }}>Share on Twitter</button>
+            </div>
+
         </div>
     );
 };
