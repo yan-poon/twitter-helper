54aaf666a931c24a6db4d7dca809c02ed26bd019
Cavour Poon <cavouriypoon@outlook.com>
2025-01-05 17:06:13 +0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NewsSearch from './components/NewsSearch/NewsSearch';
+import About from './components/About/About';
 
 function App() {
   return (
-    <div className="App">
-        <div className="App">
-          <NewsSearch />
-        </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+        <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/news-search" element={<NewsSearch />} />
+          <Route path="*" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the About page of the Twitter Helper application.</p>
+        </div>
+    );
+};
+
+export default About;
