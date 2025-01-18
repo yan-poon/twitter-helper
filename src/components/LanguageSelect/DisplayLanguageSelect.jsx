6f9f7984cd375512e07b52b9cc69df0978e25970
Cavour Poon <cavouriypoon@outlook.com>
2025-01-19 02:56:52 +0800

    Support more language

--- src/components/LanguageSelect/DisplayLanguageSelect.jsx
+++ src/components/LanguageSelect/DisplayLanguageSelect.jsx
@@ -12,8 +12,12 @@ const DisplayLanguageSelector = () => {
         <div className="language-select-container">
             <select onChange={changeLanguage} defaultValue={i18n.language}>
                 <option value="en">English</option>
-                <option value="zh-HK">繁體中文</option>
+                <option value="zh-TW">繁體中文</option>
                 <option value="zh-CN">简体中文</option>
+                <option value="es">Español</option>
+                <option value="ja">日本語</option>
+                <option value="fr">Français</option>
+                <option value="de">Deutsch</option>
             </select>
         </div>
     );
